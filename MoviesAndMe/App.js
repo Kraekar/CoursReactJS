ea5079db7b4ccbf24eed1677b812cbcf3ab3b37e
KNR <regn.natha@gmail.com>
2019-02-01 15:01:42 +0100

    début app films

--- MoviesAndMe/App.js
+++ MoviesAndMe/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Search from './Components/Search.js'
+// App.js
+
+import React from 'react'
+import Search from './Components/Search'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Search/>
-    );
+        <Search/>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
